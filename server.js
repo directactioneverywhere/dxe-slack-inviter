a731948cccc4ef484ee26c57239037beb2414443
Alex Gleason <alex@alexgleason.me>
2016-04-29 17:20:02 -0400

    Updated readme, added comments, echo port number

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const fs = require('fs')
 const slackin = require('slackin')
 
+// Use local environment if it exists,
+// otherwise, use the OS environment
 try {
   var env = require('./env.js')
 } catch(e) {
   var env = process.env
 }
 
+const PORT = env.PORT || 3000
+
 slackin.default({
   token: env.SLACK_API_KEY,
   interval: 1000,
   org: 'dxe',
-}).listen(env.PORT || 3000)
+}).listen(PORT)
+console.log(`Listening on port ${PORT}...`)
